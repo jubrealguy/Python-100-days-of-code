749d0c282aed1143d08580e26045d6ba55167fa9
jubrealguy <whizkidjubrealguy@gmail.com>
2024-02-28 20:26:23 +0100

    completed code and final organization

--- 8_day/cipher.py
+++ 8_day/cipher.py
@@ -1,31 +1,34 @@
-def cipher(alpha, op, txt, shift):
+alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+is_op = True
+
+def cipher(op, txt, shift):
     new_txt = ""
-    if op == 'encode':
-        for letter in txt:
-            pos = alpha.index(letter)
+    for letter in txt:
+        pos = alphabets.index(letter)
+        if op == 'encode':
             new_pos = pos + shift
             if new_pos > 25:
                 a = new_pos % 26
                 new_pos = a
-            new_txt += alpha[new_pos]
-        print("Your encrypted text is {}".format(new_txt))
+            new_txt += alphabets[new_pos]
     
-    elif op == 'decode':
-        for letter in txt:
-            pos = alpha.index(letter)
+        elif op == 'decode':
             new_pos = pos - shift
             if new_pos < 0:
                 a = new_pos + 26
                 new_pos = a % 26
-            new_txt += alpha[new_pos]
-        print("Your decrypted text is {}".format(new_txt))
-    else:
-        print("wrong command")
-    
+            new_txt += alphabets[new_pos]
+    print("Your {}d text is {}".format(op, new_txt))
 
-alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-operation = input("Type encode to encrypt and type decode to decrypt: ").lower()
+while is_op:
+    operation = input("Type encode to encrypt and type decode to decrypt: ").lower()
+
+    if operation == 'encode' or operation == 'decode':
+        is_op = False
+    else:
+        print("WRONG COMMAND!!! Check instruction well")
+
 text = input("Enter text: ").lower()
 shift_num = int(input("Shift number: "))
 
-cipher(alpha=alphabets, op=operation, txt=text, shift=shift_num)
+cipher(op=operation, txt=text, shift=shift_num)
