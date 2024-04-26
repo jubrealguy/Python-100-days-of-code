534b1274f00fe88ee94a428ac39a50c8f737683b
jubrealguy <whizkidjubrealguy@gmail.com>
2024-04-26 22:33:45 +0100

    completed the program, put some reusable codes in functions

--- 11_day/blackjack.py
+++ 11_day/blackjack.py
@@ -1,48 +1,58 @@
 import random as rd
-
 cards = ["A", 2, 3, 4, 5, 6, 7, 8, 9, "K", "Q", "J"]
 
+def winner():
+    sum1 = sum(computer_choice)
+    sum2 = sum(my_choice)
+    if sum1 > sum2 or sum2 > 21:
+        print(f"Computer has {sum1}\nYou have {sum2}\nComputer wins")
+    elif sum1 == sum2:
+        print("It's a draw")
+    else:
+        print(f"Computer has {sum1}\nYou have {sum2}\nYow win!!!")
+
+def replace_with_10(card_arr):
+    for x in range(len(card_arr)):
+        if card_arr[x] == "K" or card_arr[x] == "Q" or card_arr[x] == "J":
+            card_arr[x] = 10
+
+def choice():
+    replace_with_10(my_choice)
+    for x in range(len(my_choice)):
+        if my_choice[x] == "A":
+            my_choice[x] = int(input("You picked an 'A' Do you want to replace it with 1 or 11: "))
+
+def computer():
+    computer_choice.append(rd.choice(cards))
+    replace_with_10(computer_choice)
+    print(f"Computer cards: {computer_choice}")
+    print(f"My cards: {my_choice}")
+    for x in range(len(computer_choice)):
+        if computer_choice[x] == "A":
+            computer_choice[x] = rd.choice([1, 11])
+
 computer_choice =[rd.choice(cards)]
+replace_with_10(computer_choice)
 for x in range(len(computer_choice)):
-    if computer_choice[x] == "K" or computer_choice[x] == "Q" or computer_choice[x] == "J":
-        computer_choice[x] = 10
     if computer_choice[x] == "A":
         computer_choice[x] = rd.choice([1, 11])
 
 my_choice = [rd.choice(cards) for i in range(2)]
-for x in range(len(my_choice)):
-    if my_choice[x] == "K" or my_choice[x] == "Q" or my_choice[x] == "J":
-        my_choice[x] = 10
-    if my_choice[x] == "A":
-        my_choice[x] = int(input("Do you want your A as 1 or 11: "))
+choice()
 
 print(computer_choice)
 print(my_choice)
 
 new_card = input("Type 'y' to get another card and 'n' to pass: ")
+
 if new_card == 'n':
-    sum1 = sum(computer_choice)
-    sum2 = sum(my_choice)
-    print(sum1, sum2)
+    computer()
+    winner()
 elif new_card == 'y':
-    computer_choice.append(rd.choice(cards))
     my_choice.append(rd.choice(cards))
-    
-    for x in range(len(computer_choice)):
-        if computer_choice[x] == "K" or computer_choice[x] == "Q" or computer_choice[x] == "J":
-            computer_choice[x] = 10
-        if computer_choice[x] == "A":
-            computer_choice[x] = rd.choice([1, 11])
-    for x in range(len(my_choice)):
-        if my_choice[x] == "K" or my_choice[x] == "Q" or my_choice[x] == "J":
-            my_choice[x] = 10
-        if my_choice[x] == "A":
-            my_choice[x] = int(input("Do you want your A as 1 or 11: "))
-    print(computer_choice)
-    print(my_choice)
-    sum1 = sum(computer_choice)
-    sum2 = sum(my_choice)
-    print(sum1, sum2)
+    choice()
+    computer()
+    winner()
     
 else:
     print("Thank you")
