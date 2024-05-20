c73c717b3d6784703c6fe05092ae2e6968a4b6d5
jubrealguy <whizkidjubrealguy@gmail.com>
2024-05-20 04:34:37 +0100

    worked on the game number_guessing_game

--- 12_day/number_guessing_game.py
+++ 12_day/number_guessing_game.py
@@ -1,11 +1,20 @@
 import random as rd
 
+print("""
+
+   _____                       _______ _            _   _                 _               
+  / ____|                     |__   __| |          | \ | |               | |              
+ | |  __ _   _  ___  ___ ___     | |  | |__   ___  |  \| |_   _ _ __ ___ | |__   ___ _ __ 
+ | | |_ | | | |/ _ \/ __/ __|    | |  | '_ \ / _ \ | . ` | | | | '_ ` _ \| '_ \ / _ \ '__|
+ | |__| | |_| |  __/\__ \__ \    | |  | | | |  __/ | |\  | |_| | | | | | | |_) |  __/ |   
+  \_____|\__,_|\___||___/___/    |_|  |_| |_|\___| |_| \_|\__,_|_| |_| |_|_.__/ \___|_|   
+                                                                                                                                                                                 
+""")
+
 print("Welcome to Number Guessing Game")
 print("I'm thinking of a number between 1 -100")
 
 random_num = rd.randint(1, 100)
-print(random_num)
-
 
 def guess_game(num_trials):
     more_attempts = True
@@ -14,7 +23,7 @@ def guess_game(num_trials):
         guessed_num = int(input("Make a guess: "))
         if num_trials > 1:
             if guessed_num == random_num:
-                print("You got it, you guessed right")
+                print("You got it, the answer is {}".format(random_num))
                 more_attempts = False
             elif guessed_num > random_num:
                 print(f"Too high\nGuess again")
@@ -27,10 +36,12 @@ def guess_game(num_trials):
 
 level_flag = True
 while level_flag:
-    level = input("Choose a difficulty, type 'easy' or 'hard: ")
+    level = input("Choose a difficulty, type 'easy' or 'hard: ").lower()
     if level == 'easy':
         guess_game(10)
         level_flag = False
     elif level == 'hard':
         guess_game(5)
         level_flag = False
+    else:
+        print("Wrong input, type 'easy' or 'hard: ")
