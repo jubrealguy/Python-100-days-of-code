1f9772855ceb0da6ab8a8532fa13f257c16e991b
jubrealguy <whizkidjubrealguy@gmail.com>
2024-05-30 23:10:33 +0100

    reworked the code and added docstrings

--- 15_day/coffee_machine.py
+++ 15_day/coffee_machine.py
@@ -1,6 +1,7 @@
 from data import MENU, resources
 
-def input_amount():
+def process_coins():
+    """ A function that process coins and returns the total amount paid"""
     print("please insert coins")
     quarters = int(input("How many quarters: ")) * 0.25
     dimes = int(input("How many dimes: ")) * 0.1
@@ -11,7 +12,9 @@ def input_amount():
 
 money = 0
 resources["money"] = money
+
 def output(coffee):
+    """ Function to check if transaction was successful and subsequently make the coffee """
     if coffee == "latte" or coffee == "cappuccino":
         resources["milk"] -= MENU[coffee]["ingredients"]["milk"]
     resources["water"] -= MENU[coffee]["ingredients"]["water"]
@@ -20,35 +23,35 @@ def output(coffee):
     money = MENU[coffee]["cost"]
     resources["money"] += money
 
-    change = input_amount() - money
+    change = process_coins() - money
     change = round(change, 2)
+
+    def check_ingredients(item):
+        """ Function to check if ingredients are enough """
+        if resources[item] <= 0:
+            resources[item] = 0
+            print(f"There is no enough {item}")
 
     if change >= 0 and resources["milk"] > 0 and resources["water"] > 0 and resources["coffee"] > 0:
         print(f"you have {change} in change")
-        print(f"Here is your {coffee}. Enjoy!!!")
+        print(f"Here is your {coffee} ☕️ Enjoy!!!")
     elif change < 0:
         print("That is not enough money. Money refunded")
-    elif resources["milk"] <= 0:
-        print("There is no enough milk")
-    elif resources["water"] <= 0:
-        print("There is no enough water")
-    elif resources["coffee"] <= 0:
-        print("There is no enough coffee")
+    else:
+        check_ingredients("milk")
+        check_ingredients("water")
+        check_ingredients("coffee")
 
 should_continue = True
 while should_continue:
     coffee_type = input("What coffee would you like? (espresso/latte/cappuccino): ").lower()
-    if coffee_type == "report":
+    if coffee_type == "off":
+        should_continue = False
+    elif coffee_type in ["espresso", "latte", "cappuccino"]:
+        output(coffee_type)
+    elif coffee_type == "report":
         for key, value in resources.items():
             print(f"{key}: {value}")
-    elif coffee_type == "espresso":
-        output("espresso")
-    elif coffee_type == "latte":
-        output("latte")
-    elif coffee_type == "cappuccino":
-        output("cappuccino")
-    elif coffee_type == "off":
-        should_continue = False
     else:
         print("Not available, please ")
 
