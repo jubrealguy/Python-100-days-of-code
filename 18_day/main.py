4734c348b03dd963000d59e2a48b4a5382b7b05d
jubrealguy <whizkidjubrealguy@gmail.com>
2024-06-03 20:11:03 +0100

    turtle project

--- 18_day/main.py
+++ 18_day/main.py
@@ -1,6 +1,26 @@
 from turtle import Turtle, Screen
 
 snake = Turtle()
+snake.shape("turtle")
+snake.color("red")
+for i in range(4):
+    snake.forward(200)
+    snake.right(90)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 screen = Screen()
