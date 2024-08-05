2de079c4f878766ac19fd25944ca251121aae348
jubrealguy <whizkidjubrealguy@gmail.com>
2024-08-05 21:29:54 +0100

    completed letter

--- 24_day/Mail_merge/main.py
+++ 24_day/Mail_merge/main.py
@@ -1,9 +1,15 @@
-with open('./Input/Names/invited_names.txt') as names:
-    name = names.readlines()
+PLACEHOLDER = "[name]"
 
-for name in names:
-    with open('./Input/Letters/starting_letter.txt') as letter:
-        with open(f"./Output/ReadyToSend/letter_to_{name}.txt", w) as file:
+with open('./Input/Names/invited_names.txt') as names:
+    name_list = names.readlines()
+
+with open('./Input/Letters/starting_letter.txt') as letter:
+    content = letter.read()
+    for name in name_list:  
+        stripped_name = name.strip()
+        new_letter = content.replace(PLACEHOLDER, stripped_name)
+        with open(f"./Output/ReadyToSend/letter_to_{stripped_name}.txt", mode = 'w') as file:
+            file.write(new_letter)
             
 
 
