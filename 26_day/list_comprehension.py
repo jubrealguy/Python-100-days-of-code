f309b43aafaadb900bf765b5ac7f384d31c3f501
jubrealguy <whizkidjubrealguy@gmail.com>
2024-08-23 18:33:44 +0100

    day26

---
+++ 26_day/list_comprehension.py
@@ -0,0 +1,10 @@
+numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+squared_numbers = [num ** 2 for num in numbers]
+even_numbers = [num for num in numbers if num % 2 == 0]
+print(squared_numbers)
+print(even_numbers)
+
+
+names = ["Ola", "Bayo", "Titi", "Dayo", "Tosin", "Tolu"]
+
+student_scores = {}
