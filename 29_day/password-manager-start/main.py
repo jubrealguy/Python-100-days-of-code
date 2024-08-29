fe226613139d6e1387d9842f02018b2924f74001
jubrealguy <whizkidjubrealguy@gmail.com>
2024-08-29 20:50:19 +0100

    user interface of the password generator

---
+++ 29_day/password-manager-start/main.py
@@ -0,0 +1,42 @@
+from tkinter import *
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.config(padx=50, pady=50)
+window.title("Password Generator")
+
+canvas =Canvas(width=200, height=200)
+padlock = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=padlock)
+canvas.grid(row=0, column=1)
+
+website_label = Label(text="Website:")
+website_label.grid(row=1, column=0)
+website_input = Entry(width=35)
+website_input.grid(row=1, column=1, columnspan=2)
+
+email_label = Label(text="Email/Username:")
+email_label.grid(row=2, column=0)
+email_input = Entry(width=35)
+email_input.grid(row=2, column=1, columnspan=2)
+
+password_label = Label(text="Password:")
+password_label.grid(row=3, column=0)
+password_input = Entry(width=35)
+password_input.grid(row=3, column=1)
+
+generate_button = Button(text="Generate Password")
+generate_button.grid(row=3, column=2)
+
+add_button = Button(text="Add", width=36)
+add_button.grid(row=4, column=1, columnspan=2)
+
+
+
+
+
+window.mainloop()
