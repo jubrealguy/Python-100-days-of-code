d0a5588585d0d089d9c51a902c2602f822fab46c
jubrealguy <whizkidjubrealguy@gmail.com>
2024-08-30 05:24:15 +0100

    modified the UI

--- 29_day/password-manager-start/main.py
+++ 29_day/password-manager-start/main.py
@@ -17,26 +17,22 @@ canvas.grid(row=0, column=1)
 website_label = Label(text="Website:")
 website_label.grid(row=1, column=0)
 website_input = Entry(width=35)
-website_input.grid(row=1, column=1, columnspan=2)
+website_input.grid(row=1, column=1, columnspan=2, sticky="W")
 
 email_label = Label(text="Email/Username:")
 email_label.grid(row=2, column=0)
 email_input = Entry(width=35)
-email_input.grid(row=2, column=1, columnspan=2)
+email_input.grid(row=2, column=1, columnspan=2, sticky="W")
 
 password_label = Label(text="Password:")
 password_label.grid(row=3, column=0)
-password_input = Entry(width=35)
-password_input.grid(row=3, column=1)
+password_input = Entry(width=27)
+password_input.grid(row=3, column=1, sticky="W")
 
 generate_button = Button(text="Generate Password")
-generate_button.grid(row=3, column=2)
+generate_button.grid(row=3, column=2, sticky="W")
 
 add_button = Button(text="Add", width=36)
-add_button.grid(row=4, column=1, columnspan=2)
-
-
-
-
+add_button.grid(row=4, column=1, columnspan=2, sticky="W")
 
 window.mainloop()
