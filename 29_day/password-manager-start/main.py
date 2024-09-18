9b2a67c5d5ca1db81e4b214765f40af2967911e6
jubrealguy <whizkidjubrealguy@gmail.com>
2024-09-18 16:45:07 +0100

    password generator

--- 29_day/password-manager-start/main.py
+++ 29_day/password-manager-start/main.py
@@ -1,8 +1,44 @@
 from tkinter import *
+from tkinter import messagebox
+from random import choice, shuffle, randint
+import pyperclip
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+#Password Generator Project
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    password_list = [choice(letters) for char in range(randint(8, 10))]
+    password_list += [choice(symbols) for char in range(randint(2, 4))]
+    password_list += [choice(numbers) for char in range(randint(2, 4))]
+
+    shuffle(password_list)
+
+    password = "".join(password_list)
+    password_input.insert(0, password)
+    pyperclip.copy(password)
+
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+    website = website_input.get()
+    email = email_input.get()
+    password = password_input.get()
+
+    if len(website) == 0 or len(password) == 0:
+        messagebox.showinfo(title='Oops!!!!!', message='You left a box empty')
+    else:
+        is_ok = messagebox.askokcancel(title='Title', message=f'These are details entered:\nEmail: {email}\nPassword: {password}\nIs it okay to save?')
+
+        if is_ok:
+            with open("data.txt", "a") as file:
+                file.write(f"{website} | {email} | {password}\n")
+                website_input.delete(0, END)
+                password_input.delete(0, END)
+
+    
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -16,24 +52,27 @@ canvas.grid(row=0, column=1)
 
 website_label = Label(text="Website:")
 website_label.grid(row=1, column=0)
-website_input = Entry(width=35)
-website_input.grid(row=1, column=1, columnspan=2)
+website_input = Entry(width=45)
+website_input.grid(row=1, column=1, columnspan=2, sticky="W")
+website_input.focus()
 
 email_label = Label(text="Email/Username:")
 email_label.grid(row=2, column=0)
-email_input = Entry(width=35)
-email_input.grid(row=2, column=1, columnspan=2)
+email_input = Entry(width=45)
+email_input.grid(row=2, column=1, columnspan=2, sticky="W")
+email_input.insert(0, 'whizkidjubrealguy@gmail.com')
+
 
 password_label = Label(text="Password:")
 password_label.grid(row=3, column=0)
-password_input = Entry(width=35)
-password_input.grid(row=3, column=1)
+password_input = Entry(width=33)
+password_input.grid(row=3, column=1, sticky="W")
 
-generate_button = Button(text="Generate Password")
-generate_button.grid(row=3, column=2)
+generate_button = Button(text="Generate Password", command=generate_password)
+generate_button.grid(row=3, column=2, sticky="W")
 
-add_button = Button(text="Add", width=36)
-add_button.grid(row=4, column=1, columnspan=2)
+add_button = Button(text="Add", width=38, command=save)
+add_button.grid(row=4, column=1, columnspan=2, sticky="W")
 
 
 
