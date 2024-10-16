994338558053ffa890f241cf8df07ba903bb60fd
jubrealguy <whizkidjubrealguy@gmail.com>
2024-10-16 15:48:15 +0100

    completed the flash card capstone project

--- 31_day/flash-card-project-start/main.py
+++ 31_day/flash-card-project-start/main.py
@@ -3,26 +3,49 @@ import pandas as pd
 import random as rd
 
 BACKGROUND_COLOR = "#B1DDC6"
+current_card = {}
+lang_dict = {}
+
+try:
+    data = pd.read_csv('./data/words_to_learn.csv')
+except FileNotFoundError:
+    original_data = pd.read_csv('./data/french_words.csv')
+    lang_dict = original_data.to_dict(orient='records')
+else:
+    lang_dict = data.to_dict(orient='records')
+
+def next_card():
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
+    current_card = rd.choice(lang_dict)
+    canvas.itemconfig(card_title, text = "French", fill="black")
+    canvas.itemconfig(card_word, text = current_card["French"], fill="black")
+    canvas.itemconfig(card_background, image=card_front_image)
+    flip_timer = window.after(3000, func=flip_card)
+
+def flip_card():
+    canvas.itemconfig(card_title, text = "English", fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+    canvas.itemconfig(card_background, image=card_back_image)
+
+def is_known():
+    lang_dict.remove(current_card)
+    data = pd.DataFrame(lang_dict)
+    data.to_csv("./data/words_to_learn.csv", index=False)
+    next_card()
 
 window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
-title = 'French'
-data = pd.read_csv('./data/french_words.csv')
-lang_dict = data.to_dict(orient='records')
-
-def next_card():
-    current_card = rd.choice(lang_dict)
-    canvas.itemconfig(card_title, text = "French")
-    canvas.itemconfig(card_word, text = current_card["French"])
+flip_timer = window.after(3000, func=flip_card)
 
 canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 card_front_image = PhotoImage(file="./images/card_front.png")
+card_back_image = PhotoImage(file='./images/card_back.png')
 
-canvas.create_image(400, 263 , image=card_front_image)
-card_title = canvas.create_text(400, 150, text=title, font=('Arial', 40, 'italic'))
-
+card_background = canvas.create_image(400, 263 , image=card_front_image)
+card_title = canvas.create_text(400, 150, text="French", font=('Arial', 40, 'italic'))
 card_word = canvas.create_text(400, 263, text="word", font=('Arial', 60, 'bold'))
 canvas.grid(row=0, column=0, columnspan=2)
 
@@ -31,7 +54,7 @@ wrong_button = Button(image=wrong_image, highlightthickness=0, command=next_card
 wrong_button.grid(row=1, column=0)
 
 right_image = PhotoImage(file="./images/right.png")
-right_button = Button(image=right_image, highlightthickness=0, command=next_card)
+right_button = Button(image=right_image, highlightthickness=0, command=is_known)
 right_button.grid(row=1, column=1)
 
 next_card()
