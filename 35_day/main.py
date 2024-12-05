16738234f0710d64b83c54d5b7c3c65c008ba70b
jubrealguy <whizkidjubrealguy@gmail.com>
2024-12-05 15:46:23 +0100

    weather data with api call

---
+++ 35_day/main.py
@@ -0,0 +1,17 @@
+# Weather API  ---   9111648e8bbe0ba6f3e54eff0330f3db
+
+import requests
+
+
+OWM = 'https://api.openweathermap.org/data/2.5/weather'
+API_KEY = '9111648e8bbe0ba6f3e54eff0330f3db'
+
+weather_params = {
+    'lat': 6.5244,
+    'lon': 3.3792,
+    'appid': API_KEY
+}
+
+response = requests.get(OWM, params=weather_params)
+print(response.status_code)
+print(response.json())
