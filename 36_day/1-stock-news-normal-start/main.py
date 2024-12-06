72e306078db3d53fe65c58726ba7ace2a7580205
jubrealguy <whizkidjubrealguy@gmail.com>
2024-12-06 18:01:36 +0100

    latest

--- 36_day/1-stock-news-normal-start/main.py
+++ 36_day/1-stock-news-normal-start/main.py
@@ -9,10 +9,7 @@ COMPANY_NAME = "Tesla Inc"
 STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 
-STOCK_API_KEY = "V3U87QK9IEHPJYP8"
-NEWS_API_KEY = "77f6c7d42fa44702b10e1ad9fa04cf8d"
-TWILIO_SID = ''
-TWILIO_AUTH_TOKEN = ''
+
 
     ## STEP 1: Use https://www.alphavantage.co/documentation/#daily
 # When stock price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
