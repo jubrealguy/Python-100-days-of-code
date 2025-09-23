6e04edb7a1f15e22f8d2e31a2e00fda0e01c6241
jubrealguy <whizkidjubrealguy@gmail.com>
2025-09-24 00:39:51 +0100

    beautiful soup to get top 100 movies

---
+++ 45_day/start/movies.py
@@ -0,0 +1,13 @@
+from bs4 import BeautifulSoup
+import requests
+
+res = requests.get("https://www.empireonline.com/movies/features/best-movies-2/")
+web = res.text
+soup = BeautifulSoup(web, "html.parser")
+
+movies = soup.select("h2 strong")
+all_movies = [movie.getText() for movie in movies]
+for movie in all_movies[::-1]:
+    print(movie)
+
+print(len(all_movies))
